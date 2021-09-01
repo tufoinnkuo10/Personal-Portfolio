cf94e852a799db0c6e5be5339b07667547c52bca
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-09-01 11:14:44 +0100

    add preliminaries and close button

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ window.onload = () => {
 
   burger.addEventListener('click', (event) => {
     event.preventDefault();
+    console.log("")
     const mobileMenu = document.querySelector('.mobile-menu');
+    console.log(mobileMenu)
     mobileMenu.classList.add('show');
   });
 
@@ -24,3 +26,67 @@ window.onload = () => {
 
   
 };
+
+
+
+
+
+//content description
+
+const projectContent = [
+  {
+    Image: "./image/ref.png",
+    description: 'A daily selection of privately personalized!',
+    name: 'multi post stories',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    links: ['https://tufoinnkuo10.github.io/Personal-Portfolio', 'https://github.com/tufoinnkuo10/Personal-Portfolio'],
+  },
+  {
+    Image: './image/prof-print.png',
+    description: ' A daily selection of privatel personalized reads; no accountssign-ups required. Has been the industry standard',
+    name: 'card 2',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    links: ['https://tufoinnkuo10.github.io/Personal-Portfolio', 'https://github.com/tufoinnkuo10/Personal-Portfolio'],
+  },
+  {
+    Image: './image/dash-data.png',
+    description: 'A daily selection of privatel personalized reads; no accountssign-ups required. Has been the industry standard!',
+    name: 'card 3',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    links: ['https://tufoinnkuo10.github.io/Personal-Portfolio', 'https://github.com/tufoinnkuo10/Personal-Portfolio'],
+  },
+  {
+    Image: './image/dash-data.png',
+    description: 'A daily selection of privatel personalized reads; no accountssign-ups required. Has been the industry standard',
+    name: 'card 4',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    links: ['https://tufoinnkuo10.github.io/Personal-Portfolio', 'https://github.com/tufoinnkuo10/Personal-Portfolio'],
+  },
+  {
+    Image: './image/websitepro.png',
+    description: 'A daily selection of privatel personalized reads; no accountssign-ups required. Has been the industry standard',
+    name: 'card 5',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    links: ['https://tufoinnkuo10.github.io/Personal-Portfolio', 'https://github.com/tufoinnkuo10/Personal-Portfolio'],
+  },
+  {
+    featuredImage: './image/websitepro.png',
+    description: 'A daily selection of privatel personalized reads; no accountssign-ups required. Has been the industry standard',
+    name: 'card 6',
+    technologies: ['Ruby on rails', 'css', 'JavaScript'],
+    links: ['https://tufoinnkuo10.github.io/Personal-Portfolio', 'https://github.com/tufoinnkuo10/Personal-Portfolio'],
+  },
+];
+
+const cproject = document.querySelectorAll('.cproject');
+cproject.forEach((element) => {
+  
+  element.addEventListener("click", ()=>{
+    const popup = document.querySelector(
+      '.pop-up'
+    );
+    console.log(popup);
+    popup.style.display = 'block';
+  })
+});
+console.log(cproject);
