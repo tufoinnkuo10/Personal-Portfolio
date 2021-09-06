78ced7f26963e668e29588a91ad8c39c23c4c064
tufoinnkuo10 <tufoinnkuo10@gmail.com>
2021-09-06 10:48:12 +0100

    modify mobile menu to popup and disappear when closed,

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ window.onload = () => {
   burger.addEventListener('click', (event) => {
     event.preventDefault();
     const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.classList.add('show');
+    mobileMenu.classList.add('showing');
   });
 
   const iconx = document.querySelector('.iconx');
 
   iconx.addEventListener('click', () => {
     const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.classList.remove('show');
+    mobileMenu.classList.remove('showing');
   });
 
   const mobileMenuList = document.querySelector('.mobile-menu ul');
 
   mobileMenuList.addEventListener('click', () => {
     const mobileMenu = document.querySelector('.mobile-menu');
-    mobileMenu.classList.remove('show');
+    mobileMenu.classList.remove('showing');
   });
 };
