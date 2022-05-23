530318e86162c05f2c5b32b92a98cad80dd230f5
Tufoin Nkuo <tufoinnkuo10@gmail.com>
2022-05-23 08:12:37 +0100

    add project pics to app

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ const projectContent = [
     ],
   },
   {
-    Image: './image/rocketapp.png',
+    Image: './image/rocketapp2.png',
     description:
       'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
     name: 'card 5',
